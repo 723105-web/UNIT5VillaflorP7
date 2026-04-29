5890007eb7972c117fc8944d08058ab54dff3bf3
D1luffy <723105@student.dusd.net>
2026-04-29 15:25:09 -0700

    Objects to scene
    I added 3 good objects and 1 bad and put a script on them all with rigidbody and box collider

---
+++ P7VillaflorUnit5/Assets/Scripts/Target.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+       
+        targetRb.AddForce(Vector3.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
+        
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
