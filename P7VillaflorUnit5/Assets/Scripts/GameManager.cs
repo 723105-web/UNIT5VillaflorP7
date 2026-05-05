0a8ed7a74511cecb3bf6cab53d377a97ea74977b
D1luffy <723105@student.dusd.net>
2026-05-05 15:20:58 -0700

    Score and gameover text
    I was able to add score text whenever i click on the objects and added the gameover text

--- P7VillaflorUnit5/Assets/Scripts/GameManager.cs
+++ P7VillaflorUnit5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    private int score;
     private float spawnRate = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -27,5 +35,10 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+   public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
